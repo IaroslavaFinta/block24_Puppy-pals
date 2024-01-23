3cdb67f60422433be9760fd3d7e2a52606fc4ca6
Iaroslava Finta <iaroslava.sv7@gmail.com>
2024-01-23 15:25:15 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default App
